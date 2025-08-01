137ddb57c4b3a763dcb6d72f92a3e559ef1dd7c7
Zahid Tariq <dihazqirat@gmail.com>
2025-08-02 00:01:12 +0530

    Add average salary by department chart

--- hr_dashboard.py
+++ hr_dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 st.title('Hr Dashboard')
 df = pd.read_csv('hr_data.csv')
 df.columns = df.columns.str.strip().str.replace(' ','')
@@ -16,3 +17,11 @@ col1,col2,col3 = st.columns(3)
 col1.metric('Total_Employees',total_employees)
 col2.metric('Current_Employees',current_employees)
 col3.metric('Former_Employees',former_employees)
+avg_salary = df[df['Status'] == 'Current'].groupby('Department')['Salary'].mean().reset_index()
+fig, ax = plt.subplots()
+ax.bar(avg_salary['Department'], avg_salary['Salary'],color="skyblue")
+ax.set_title('Average Salary by Department')
+ax.set_xlabel('Department')
+ax.set_ylabel('Average Salary')
+plt.xticks(rotation=45)
+st.pyplot(fig)
