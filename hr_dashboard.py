e828cca3aae543962e9fb5097efc2d1bab31bfa2
Zahid Tariq <dihazqirat@gmail.com>
2025-08-02 00:42:54 +0530

    Fix: column name conflict for gender layout

--- hr_dashboard.py
+++ hr_dashboard.py
@@ -31,9 +31,9 @@ gender_counts = df[df["Status"] == 'Current']['Gender'].value_counts()
 male_count = gender_counts.get('Male', 0)
 female_count = gender_counts.get('Female', 0)
 
-col1, col2 = st.columns(2)
-col1.metric('Total Male Employees',male_count)
-col2.metric('Total Female Employees',female_count)
+gcol1, gcol2 = st.columns(2)
+gcol1.metric('Total Male Employees', male_count)
+gcol2.metric('Total Female Employees', female_count)
 
 fig2, ax2 = plt.subplots()
 ax2.pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', colors=['lightblue', 'lightpink'])
